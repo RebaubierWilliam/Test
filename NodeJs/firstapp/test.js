f23bf8761f587fae728593f898bfb11d4482d68d
RebaubierWilliam <RebaubierWilliam@gmail.com>
2015-11-26 18:14:28 +0100

    ajout des exemple nodeJs

---
+++ NodeJs/firstapp/test.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+});
+
+// ... Tout le code de gestion des routes (app.get) se trouve au-dessus
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
+app.listen(8080);
